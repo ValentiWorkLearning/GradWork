0a2a7dafa15fe1cd0eaecca0c03abb889a17be29
Valentyn Korniienko <kornienko-vr@rambler.ru>
2019-11-20 21:49:30 +0200

    Experimental: connectable example from NRF52 Tutorial

--- Firmware/nrf52_e73mod_cmake/Firmware/src/drivers/ble/ble_custom_service.hpp
+++ Firmware/nrf52_e73mod_cmake/Firmware/src/drivers/ble/ble_custom_service.hpp
@@ -1,59 +1,108 @@
-#pragma once
+#ifndef BLE_CUS_H__
+#define BLE_CUS_H__
 
+#include <stdint.h>
+#include <stdbool.h>
 #include "ble.h"
 #include "ble_srv_common.h"
 
-#include "MetaUtils.hpp"
+/**@brief   Macro for defining a ble_hrs instance.
+ *
+ * @param   _name   Name of the instance.
+ * @hideinitializer
+ */
+#define BLE_CUS_DEF(_name)                                                                          \
+static ble_cus_t _name;                                                                             \
+NRF_SDH_BLE_OBSERVER(_name ## _obs,                                                                 \
+                     BLE_HRS_BLE_OBSERVER_PRIO,                                                     \
+                     ble_cus_on_ble_evt, &_name)
 
-#include <cstdint>
-#include <array>
 
-#include "Noncopyable.hpp"
 
-namespace Ble::CustomService
+// CUSTOM_SERVICE_UUID_BASE f364adc9-b000-4042-ba50-05ca45bf8abc
+
+#define CUSTOM_SERVICE_UUID_BASE         {0xBC, 0x8A, 0xBF, 0x45, 0xCA, 0x05, 0x50, 0xBA, \
+                                          0x40, 0x42, 0xB0, 0x00, 0xC9, 0xAD, 0x64, 0xF3}
+
+#define CUSTOM_SERVICE_UUID               0x1400
+#define CUSTOM_VALUE_CHAR_UUID            0x1401
+																					
+/**@brief Custom Service event type. */
+typedef enum
 {
-    static constexpr std::uint8_t UuidSize = 16;
+    BLE_CUS_EVT_NOTIFICATION_ENABLED,                             /**< Custom value notification enabled event. */
+    BLE_CUS_EVT_NOTIFICATION_DISABLED,                             /**< Custom value notification disabled event. */
+    BLE_CUS_EVT_DISCONNECTED,
+    BLE_CUS_EVT_CONNECTED
+} ble_cus_evt_type_t;
 
-    static constexpr std::array<std::uint8_t,UuidSize> UuidBase_BE = {
-            0xF3, 0x64,0xAD,0xC9
-        ,   0xB0, 0x00
-        ,   0x40, 0x42
-        ,   0xBA, 0x50
-        ,   0x05, 0xCA,0x45,0xBF, 0x8A, 0xBC
-    };
+/**@brief Custom Service event. */
+typedef struct
+{
+    ble_cus_evt_type_t evt_type;                                  /**< Type of event. */
+} ble_cus_evt_t;
 
-    static constexpr std::array CustomServiceUuid{ Meta::reverseArray( UuidBase_BE ) };
-    static constexpr std::uint8_t ServiceType = BLE_UUID_TYPE_VENDOR_BEGIN;
-
-    static constexpr std::uint16_t ServiceUuid = 0x1400;
-    static constexpr std::uint16_t ValueCharUuid = 0x1401;
+// Forward declaration of the ble_cus_t type.
+typedef struct ble_cus_s ble_cus_t;
 
 
-    class CustomService
-        :   private Utils::noncopyable
-    {
+/**@brief Custom Service event handler type. */
+typedef void (*ble_cus_evt_handler_t) (ble_cus_t * p_bas, ble_cus_evt_t * p_evt);
 
-        public:
+/**@brief Battery Service init structure. This contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
+{
+    ble_cus_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Custom Service. */
+    uint8_t                       initial_custom_value;           /**< Initial custom value */
+    ble_srv_cccd_security_mode_t  custom_value_char_attr_md;     /**< Initial security level for Custom characteristics attribute */
+} ble_cus_init_t;
 
-        CustomService();
-        ~CustomService() = default;
+/**@brief Custom Service structure. This contains various status information for the service. */
+struct ble_cus_s
+{
+    ble_cus_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Custom Service. */
+    uint16_t                      service_handle;                 /**< Handle of Custom Service (as provided by the BLE stack). */
+    ble_gatts_char_handles_t      custom_value_handles;           /**< Handles related to the Custom Value characteristic. */
+    uint16_t                      conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
+    uint8_t                       uuid_type; 
+};
 
-        private:
+/**@brief Function for initializing the Custom Service.
+ *
+ * @param[out]  p_cus       Custom Service structure. This structure will have to be supplied by
+ *                          the application. It will be initialized by this function, and will later
+ *                          be used to identify this particular service instance.
+ * @param[in]   p_cus_init  Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
+ */
+uint32_t ble_cus_init(ble_cus_t * p_cus, const ble_cus_init_t * p_cus_init);
 
-        void initCustomService();
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @details Handles all events from the BLE stack of interest to the Battery Service.
+ *
+ * @note 
+ *
+ * @param[in]   p_cus      Custom Service structure.
+ * @param[in]   p_ble_evt  Event received from the BLE stack.
+ */
+void ble_cus_on_ble_evt( ble_evt_t const * p_ble_evt, void * p_context);
 
-        void initAdvertisment();
+/**@brief Function for updating the custom value.
+ *
+ * @details The application calls this function when the cutom value should be updated. If
+ *          notification has been enabled, the custom value characteristic is sent to the client.
+ *
+ * @note 
+ *       
+ * @param[in]   p_bas          Custom Service structure.
+ * @param[in]   Custom value 
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
 
-        void initCustomCharacteric();
+uint32_t ble_cus_custom_value_update(ble_cus_t * p_cus, uint8_t custom_value);
 
-        private:
-
-        std::uint16_t m_serviceHandle;
-
-        ble_gatts_char_handles_t m_customValueHandles;   
-
-        std::uint16_t m_connectionHandle;
-
-        std::uint8_t m_uuidType;
-    };
-}
+#endif // BLE_CUS_H__
