1f63f03e224de248a5bee1705552f458439ba024
Valentyn Korniienko <kornienko-vr@rambler.ru>
2020-01-12 19:06:28 +0200

    Fixed main clocks view. Added main.cpp [SIMULATOR]

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -1,93 +1,10 @@
-#include <thread>
-#include <chrono>
+#include "ap_application.hpp"
 
-#include "lvgl/lvgl.h"
-#include "lv_drivers/display/monitor.h"
-#include "lv_drivers/indev/mouse.h"
-#include "lv_drivers/indev/keyboard.h"
-
-#include "lvgl_ui.hpp"
-
-void initHal();
-
-static lv_indev_t * kb_indev;
-
-int main(  )
+int main( )
 {
-    /*Initialize LittlevGL*/
-    lv_init();
+    Application mainApp;
 
-    /*Initialize the HAL for LittlevGL*/
-    initHal();
-
-    LvglUi::createWidgetsDemo();
-
-    while (true)
-    {
-        /* Periodically call the lv_task handler.
-        * It could be done in a timer interrupt or an OS task too.*/
-        lv_task_handler();
-        std::this_thread::sleep_for(
-            std::chrono::milliseconds(5)
-        );
-    }
-
+    mainApp.runApplicationLoop();
 
   return 0;
-}
-
-
-static void initHal(void)
-{
-    /* Add a display
-    * Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
-    monitor_init();
-
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
-
-    static lv_disp_buf_t disp_buf1;
-    static lv_color_t buf1_1[LV_HOR_RES_MAX*LV_VER_RES_MAX];
-    lv_disp_buf_init(&disp_buf1, buf1_1, nullptr, LV_HOR_RES_MAX*LV_VER_RES_MAX);
-
-    disp_drv.buffer = &disp_buf1;
-    disp_drv.flush_cb = monitor_flush;
-    lv_disp_drv_register(&disp_drv);
-
-    /* Add the mouse (or touchpad) as input device
-    * Use the 'mouse' driver which reads the PC's mouse*/
-    mouse_init();
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);          /*Basic initialization*/
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = mouse_read;         /*This function will be called periodically (by the library) to get the mouse position and state*/
-    lv_indev_drv_register(&indev_drv);
-
-    /* If the PC keyboard driver is enabled in`lv_drv_conf.h`
-    * add this as an input device. It might be used in some examples. */
-#if USE_KEYBOARD
-    lv_indev_drv_t kb_drv;
-    lv_indev_drv_init(&kb_drv);
-    kb_drv.type = LV_INDEV_TYPE_KEYPAD;
-    kb_drv.read_cb = keyboard_read;
-    kb_indev = lv_indev_drv_register(&kb_drv);
-#endif
-
-    /* Tick init.
-    * You have to call 'lv_tick_inc()' in every milliseconds
-    * Create an SDL thread to do this*/
-    auto tickThread = std::thread(
-        []
-        {
-            while (true)
-            {
-                lv_tick_inc(5);
-                std::this_thread::sleep_for(
-                    std::chrono::milliseconds(5)
-                );
-            }
-
-        }
-    );
-    tickThread.detach();
 }
