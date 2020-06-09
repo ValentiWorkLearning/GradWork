a06ccbd64bdd846a1020f63efb816094adebc086
Valentyn Korniienko <kornienko-vr@rambler.ru>
2020-06-09 13:44:25 +0300

    Added generation for binray files [CMAKE]

--- Firmware/cmake/FindNordicSDK.cmake
+++ Firmware/cmake/FindNordicSDK.cmake
@@ -254,6 +254,14 @@ target_sources(
     ${NordicCommonSrc}
 )
 
+function (createBinAndHexFiles TARGET_NAME)
+    add_custom_command(TARGET ${TARGET_NAME}
+        POST_BUILD
+        COMMAND ${CMAKE_SIZE_UTIL} ${TARGET_NAME}.out
+        COMMAND ${CMAKE_OBJCOPY} -O binary ${TARGET_NAME}.out "${TARGET_NAME}.bin"
+        COMMAND ${CMAKE_OBJCOPY} -O ihex ${TARGET_NAME}.out "${TARGET_NAME}.hex"
+        COMMENT "post build steps for ${TARGET_NAME}")
+endfunction()
 
 list(APPEND NRFSDK_INCLUDES ${NordicCommonInc} )
 list(APPEND NRFSDK_SOURCES ${NordicCommonSrc} )
