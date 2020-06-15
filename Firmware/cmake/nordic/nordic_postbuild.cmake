3ece4989c93685dd312b953539cb718dc4282978
Valentyn Korniienko <kornienko-vr@rambler.ru>
2020-06-15 18:37:55 +0300

    Fixed find_program for nrfjprog [CMAKE]

--- Firmware/cmake/nordic/nordic_postbuild.cmake
+++ Firmware/cmake/nordic/nordic_postbuild.cmake
@@ -18,9 +18,7 @@ function (nordicSdk_createBinAndHexFiles TARGET_NAME)
 endfunction()
 
 
-if( EXECUTE_MCU_FLASHING )
-    find_program( NRFJPROG NAMES nrfjprog PATHS $ENV{NRFJPROG_PATH} )
-endif()
+find_program( NRFJPROG NAMES nrfjprog PATHS $ENV{NRFJPROG_PATH} )
 
 function( nordicSdk_flashSoftDevice EXECUTABLE_NAME SOFTDEVICE_PATH NRF_TARGET )
     add_custom_target(FLASH_SOFTDEVICE ALL
