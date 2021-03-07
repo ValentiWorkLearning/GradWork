01b49bc4043fb82ee27bfc6d22377fcb8ee5b595
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-03-07 18:35:19 +0200

    Draft project for debug purpouse [CORO]

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -1,14 +1,28 @@
 #include "ap_application.hpp"
-#include <etl/memory.h>
+
+#include <ih/drivers/ih_display_idisplay.hpp>
+#include <ih/drivers/ih_display_driver_creator.hpp>
+#include <board/watchboard.hpp>
+
+//#include <etl/memory.h>
 //#include "utils/CoroutineExample.hpp"
 
 int main(void)
 {
+    auto pBoard = WatchBoard::createBoard();
+    auto pDisplayDriver = DisplayCreator::createDisplayDriver();
+    pDisplayDriver->initialize();
+
+    while(true)
+    {
+        pBoard->ledToggle();
+    }
     //testMe();
 
-    Application mainApp;
+//    Application mainApp;
 
-    mainApp.runApplicationLoop();
+//    mainApp.runApplicationLoop();
+
 
     return 0;
 }
