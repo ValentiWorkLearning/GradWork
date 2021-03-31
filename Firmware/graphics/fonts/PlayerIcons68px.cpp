6b330d19835803b3f942115ec09837e68429a847
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-03-31 15:48:48 +0300

    Updated fonts [LVGL]

--- Firmware/graphics/fonts/PlayerIcons68px.cpp
+++ Firmware/graphics/fonts/PlayerIcons68px.cpp
@@ -1,10 +1,14 @@
-#include "lvgl/lvgl.h"
-
 /*******************************************************************************
  * Size: 68 px
  * Bpp: 2
  * Opts: 
  ******************************************************************************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
 
 #ifndef PLAYERICONS68PX
 #define PLAYERICONS68PX 1
@@ -17,7 +21,7 @@
  *----------------*/
 
 /*Store the image of the glyphs*/
-static LV_ATTRIBUTE_LARGE_CONST const uint8_t gylph_bitmap[] = {
+static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
     /* U+E800 "" */
     0x0, 0x0, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0,
     0x0, 0x2d, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
@@ -251,9 +255,14 @@ static const lv_font_fmt_txt_cmap_t cmaps[] =
  *  ALL CUSTOM DATA
  *--------------------*/
 
+#if LV_VERSION_CHECK(8, 0, 0)
 /*Store all the custom data of the font*/
+static  lv_font_fmt_txt_glyph_cache_t cache;
+static const lv_font_fmt_txt_dsc_t font_dsc = {
+#else
 static lv_font_fmt_txt_dsc_t font_dsc = {
-    .glyph_bitmap = gylph_bitmap,
+#endif
+    .glyph_bitmap = glyph_bitmap,
     .glyph_dsc = glyph_dsc,
     .cmaps = cmaps,
     .kern_dsc = NULL,
@@ -261,7 +270,10 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
     .cmap_num = 1,
     .bpp = 2,
     .kern_classes = 0,
-    .bitmap_format = 0
+    .bitmap_format = 0,
+#if LV_VERSION_CHECK(8, 0, 0)
+    .cache = &cache
+#endif
 };
 
 
@@ -270,7 +282,11 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
  *----------------*/
 
 /*Initialize a public general font descriptor*/
+#if LV_VERSION_CHECK(8, 0, 0)
 const lv_font_t PlayerIcons68px = {
+#else
+lv_font_t PlayerIcons68px = {
+#endif
     .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
     .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
     .line_height = 64,          /*The maximum line height required by the font*/
@@ -278,8 +294,14 @@ const lv_font_t PlayerIcons68px = {
 #if !(LVGL_VERSION_MAJOR == 6 && LVGL_VERSION_MINOR == 0)
     .subpx = LV_FONT_SUBPX_NONE,
 #endif
+#if LV_VERSION_CHECK(7, 4, 0)
+    .underline_position = 1,
+    .underline_thickness = 0,
+#endif
     .dsc = &font_dsc           /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 };
+
+
 
 #endif /*#if PLAYERICONS68PX*/
 
