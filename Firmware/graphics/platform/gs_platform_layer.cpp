60c77bd595d67ee7577b3ef13dc21c305145a95f
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-06-03 13:56:53 +0300

    Removed tecnical debt from gs_platform_layer(old display driver)[CORO]

--- Firmware/graphics/platform/gs_platform_layer.cpp
+++ Firmware/graphics/platform/gs_platform_layer.cpp
@@ -6,114 +6,73 @@
 #include <utils/CallbackConnector.hpp>
 #include <utils/CoroUtils.hpp>
 
-#include <ih/drivers/ih_display_idisplay.hpp>
 #include <ih/drivers/ih_display_driver_creator.hpp>
+#include <ih/drivers/ih_display_idisplay.hpp>
 
-#if defined USE_HARDWARE_DISPLAY_BACKEND || defined USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND
+#if defined(USE_HARDWARE_DISPLAY_BACKEND)
 #include <app_timer.h>
 #include <nrf_drv_clock.h>
 
 namespace
 {
-    APP_TIMER_DEF(m_gfxEllapsedTimerId);
+APP_TIMER_DEF(m_gfxEllapsedTimerId);
 }
 
 namespace Graphics
 {
 #endif
 
+#if defined USE_HARDWARE_DISPLAY_BACKEND
+PlatformBackend::PlatformBackend() noexcept
+    : m_hardwareDisplayDriver{std::make_unique<TDisplayDriver>()} {};
 
-#if defined (USE_HARDWARE_DISPLAY_BACKEND)
-
-PlatformBackend::PlatformBackend()noexcept
-    :   m_hardwareDisplayDriver{
-            DisplayCreator::createDisplayDriver()
-        }
+void PlatformBackend::platformDependentInit(lv_disp_drv_t* _displayDriver) noexcept
 {
-}
-#endif
-
-#if defined (USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND)
-PlatformBackend::PlatformBackend()noexcept
-    :   m_hardwareDisplayDriver{
-        std::make_unique<TDisplayDriver>()
-    }
-{
-};
-#endif
-
-#if defined USE_HARDWARE_DISPLAY_BACKEND || defined USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND
-void
-PlatformBackend::platformDependentInit( lv_disp_drv_t* _displayDriver )noexcept
-{
-    getHardwareDisplayDriver()->initialize();
+    m_hardwareDisplayDriver->initialize();
 
     auto hardwareDriverCallback = cbc::obtain_connector(
-        [ this ]( lv_disp_drv_t* _displayDriver, const lv_area_t* _fillArea, lv_color_t* _colorFill )
-        {
-            getHardwareDisplayDriver()->fillRectangle(
-                    _fillArea->x1
-                ,   _fillArea->y1
-                ,   _fillArea->x2
-                ,   _fillArea->y2
-                ,   reinterpret_cast<std::uint16_t*>( _colorFill )
-            );
-        }
-    );
+        [this](lv_disp_drv_t* _displayDriver, const lv_area_t* _fillArea, lv_color_t* _colorFill) {
+            m_hardwareDisplayDriver->fillRectangle(
+                _fillArea->x1,
+                _fillArea->y1,
+                _fillArea->x2,
+                _fillArea->y2,
+                reinterpret_cast<std::uint16_t*>(_colorFill));
+        });
 
-    auto waitCallback = cbc::obtain_connector(
-        [](lv_disp_drv_t * disp_drv)
-        {
-            CoroUtils::CoroQueueMainLoop::GetInstance().processQueue();
-            Simple::Lib::ExecuteLaterPool::Instance().processQueue();
-        }
-    );
+    auto waitCallback = cbc::obtain_connector([](lv_disp_drv_t* disp_drv) {
+        CoroUtils::CoroQueueMainLoop::GetInstance().processQueue();
+        Simple::Lib::ExecuteLaterPool::Instance().processQueue();
+    });
 
-    if(!_displayDriver)
+    if (!_displayDriver)
         return;
 
     _displayDriver->flush_cb = hardwareDriverCallback;
     _displayDriver->wait_cb = waitCallback;
 
-    getHardwareDisplayDriver()->onRectArreaFilled.connect(
-        [this,_displayDriver]
-        {
-            lv_disp_flush_ready( _displayDriver );
-        }
-    );
+    m_hardwareDisplayDriver->onRectArreaFilled.connect(
+        [this, _displayDriver] { lv_disp_flush_ready(_displayDriver); });
 }
 
-void
-PlatformBackend::initPlatformGfxTimer()noexcept
+void PlatformBackend::initPlatformGfxTimer() noexcept
 {
     ret_code_t errorCode{};
 
-    auto gfxTimerCallback = cbc::obtain_connector(
-        [ this ]( void * _pContext )
-        {
-            lv_tick_inc( LvglNotificationTime );
-        }
-    );
+    auto gfxTimerCallback =
+        cbc::obtain_connector([this](void* _pContext) { lv_tick_inc(LvglNotificationTime); });
 
-    errorCode = app_timer_create(
-                &m_gfxEllapsedTimerId
-            ,   APP_TIMER_MODE_REPEATED
-            ,   gfxTimerCallback
-        );
-    APP_ERROR_CHECK( errorCode );
+    errorCode = app_timer_create(&m_gfxEllapsedTimerId, APP_TIMER_MODE_REPEATED, gfxTimerCallback);
+    APP_ERROR_CHECK(errorCode);
 
-    errorCode = app_timer_start(
-                m_gfxEllapsedTimerId
-            ,   APP_TIMER_TICKS( LvglNotificationTime )
-            ,   nullptr
-        );
-    APP_ERROR_CHECK( errorCode );
+    errorCode =
+        app_timer_start(m_gfxEllapsedTimerId, APP_TIMER_TICKS(LvglNotificationTime), nullptr);
+    APP_ERROR_CHECK(errorCode);
 }
 
-void
-PlatformBackend::executeLvTaskHandler()noexcept
+void PlatformBackend::executeLvTaskHandler() noexcept
 {
-    if(!getHardwareDisplayDriver()->isInitialized())
+    if (!m_hardwareDisplayDriver->isInitialized())
         return;
 
     lv_task_handler();
@@ -121,101 +80,76 @@ PlatformBackend::executeLvTaskHandler()noexcept
 } // namespace Graphics
 #endif
 
-
 #if defined USE_WINSDL_BACKEND
 
-#include <thread>
 #include <chrono>
+#include <thread>
 
-#include "lvgl/lvgl.h"
 #include "lv_drivers/display/monitor.h"
-#include "lv_drivers/indev/mouse.h"
 #include "lv_drivers/indev/keyboard.h"
+#include "lv_drivers/indev/mouse.h"
+#include "lvgl/lvgl.h"
 
 #include <fmt/format.h>
 
 namespace Graphics
 {
 
-PlatformBackend::PlatformBackend()noexcept = default;
+PlatformBackend::PlatformBackend() noexcept = default;
 
-void
-PlatformBackend::platformDependentInit(lv_disp_drv_t* _displayDriver)noexcept
+void PlatformBackend::platformDependentInit(lv_disp_drv_t* _displayDriver) noexcept
 {
     monitor_init();
     _displayDriver->flush_cb = monitor_flush;
 }
 
-void
-PlatformBackend::initPlatformGfxTimer()noexcept
+void PlatformBackend::initPlatformGfxTimer() noexcept
 {
-    m_tickThread = std::thread(
-        []
+    m_tickThread = std::thread([] {
+        while (true)
         {
-            while (true)
-            {
-                lv_tick_inc( LvglNotificationTime );
-                std::this_thread::sleep_for(
-                    std::chrono::milliseconds( LvglNotificationTime )
-                );
-            }
-
+            lv_tick_inc(LvglNotificationTime);
+            std::this_thread::sleep_for(std::chrono::milliseconds(LvglNotificationTime));
         }
-    );
+    });
     m_tickThread.detach();
     indevPlatformInit();
     lv_indev_drv_init(&m_indevDriver);
 }
 
-void
-PlatformBackend::indevPlatformInit()noexcept
+void PlatformBackend::indevPlatformInit() noexcept
 {
-    
+
     m_indevDriver.type = LV_INDEV_TYPE_POINTER;
     m_indevDriver.read_cb = mouse_read;
-    lv_indev_drv_register( &m_indevDriver );
+    lv_indev_drv_register(&m_indevDriver);
 
-    auto memoryMonitorTask = cbc::obtain_connector(
-        [this]( lv_timer_t* _param )
-        {
-            memoryMonitor( _param );
-        }
-    );
+    auto memoryMonitorTask =
+        cbc::obtain_connector([this](lv_timer_t* _param) { memoryMonitor(_param); });
 
-    lv_timer_create(
-            memoryMonitorTask
-        ,   10
-        ,   nullptr
-    );
+    lv_timer_create(memoryMonitorTask, 10, nullptr);
 }
 
-void
-PlatformBackend::memoryMonitor(lv_timer_t* _param)noexcept
+void PlatformBackend::memoryMonitor(lv_timer_t* _param) noexcept
 {
-    static_cast<void>( _param );
+    static_cast<void>(_param);
 
     lv_mem_monitor_t moninor{};
-    lv_mem_monitor( &moninor );
+    lv_mem_monitor(&moninor);
 
-    LOG_DEBUG_ENDL(
-            fmt::format(
-                "Used: {} , {}% fragmentation: {}, biggest free: {}"
-             ,   static_cast<std::uint32_t>( moninor.total_size - moninor.free_size )
-             ,   static_cast<std::uint32_t>( moninor.used_pct )
-             ,   static_cast<std::uint32_t>( moninor.frag_pct )
-             ,   static_cast<std::uint32_t>( moninor.free_biggest_size )
-            )
-        );
+    LOG_DEBUG_ENDL(fmt::format(
+        "Used: {} , {}% fragmentation: {}, biggest free: {}",
+        static_cast<std::uint32_t>(moninor.total_size - moninor.free_size),
+        static_cast<std::uint32_t>(moninor.used_pct),
+        static_cast<std::uint32_t>(moninor.frag_pct),
+        static_cast<std::uint32_t>(moninor.free_biggest_size)));
 }
 
-void
-PlatformBackend::executeLvTaskHandler()noexcept
+void PlatformBackend::executeLvTaskHandler() noexcept
 {
     lv_task_handler();
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds( LvglNotificationTime )
-    );
+    std::this_thread::sleep_for(std::chrono::milliseconds(LvglNotificationTime));
 }
 
-}
+} // namespace Graphics
 #endif
--- Firmware/graphics/platform/gs_platform_layer.hpp
+++ Firmware/graphics/platform/gs_platform_layer.hpp
@@ -5,22 +5,24 @@
 #include <lvgl.h>
 
 #ifdef USE_WINSDL_BACKEND
-#include <thread>
 #include <chrono>
+#include <thread>
 #endif // USE_WINSDL_BACKEND
 
-
-namespace DisplayDriver {
-    class IDisplayDriver;
+namespace DisplayDriver
+{
+class IDisplayDriver;
 }
 
-#if defined (USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND)
-    #include <display/display_coro_compact_gc9a01.hpp>
-    #include <spi/spi_wrapper_async_templated.hpp>
-    #include <backends/spi_backend_nrf.hpp>
-#endif
+#if defined(USE_HARDWARE_DISPLAY_BACKEND)
+#include <backends/spi_backend_nrf.hpp>
+#include <display/display_coro_compact_gc9a01.hpp>
+#include <spi/spi_wrapper_async_templated.hpp>
 
-#include <utils/FastPimpl.hpp>
+using TSpiBus = Interface::SpiTemplated::SpiBus<
+    Interface::SpiTemplated::NordicSpi<Interface::SpiTemplated::SpiInstance::M2>>;
+using TDisplayDriver = DisplayDriver::GC9A01Compact<TSpiBus, 240, 240>;
+#endif
 
 namespace Graphics
 {
@@ -29,56 +31,31 @@ class PlatformBackend
 {
 
 public:
+    PlatformBackend() noexcept;
 
-    PlatformBackend()noexcept;
+    void platformDependentInit(lv_disp_drv_t* _displayDriver) noexcept;
 
-    void platformDependentInit( lv_disp_drv_t* _displayDriver )noexcept;
+    void initPlatformGfxTimer() noexcept;
 
-    void initPlatformGfxTimer()noexcept;
-
-    void executeLvTaskHandler()noexcept;
+    void executeLvTaskHandler() noexcept;
 
 private:
-
     static constexpr std::uint32_t LvglNotificationTime = 15;
-#if defined USE_HARDWARE_DISPLAY_BACKEND
-    using TDisplayDriver = std::unique_ptr<DisplayDriver::IDisplayDriver>;
-#endif
-
-
-#if defined USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND
-    using TSpiBus = Interface::SpiTemplated::SpiBus<
-        Interface::SpiTemplated::NordicSpi<Interface::SpiTemplated::SpiInstance::M2>
-    >;
-    using TDisplayDriver =
-         DisplayDriver::GC9A01Compact<TSpiBus, 240, 240>;
-#endif
-
-#if defined USE_HARDWARE_DISPLAY_BACKEND || USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND
-    auto getHardwareDisplayDriver()noexcept
-    {
-        return m_hardwareDisplayDriver.get();
-    }
-#endif
-private:
-
-    void indevPlatformInit()noexcept;
-
-    void memoryMonitor(lv_timer_t* _param)noexcept;
 
 private:
+    void indevPlatformInit() noexcept;
 
-#if defined USE_HARDWARE_TEMPLATED_DISPLAY_BACKEND
+    void memoryMonitor(lv_timer_t* _param) noexcept;
+
+private:
+#if defined (USE_HARDWARE_DISPLAY_BACKEND)
     std::unique_ptr<TDisplayDriver> m_hardwareDisplayDriver;
-#elif defined  USE_HARDWARE_DISPLAY_BACKEND
-    TDisplayDriver m_hardwareDisplayDriver;
 #endif
 
 #if defined USE_WINSDL_BACKEND
     std::thread m_tickThread;
     lv_indev_drv_t m_indevDriver;
 #endif
-
 };
 
 } // namespace Graphics
