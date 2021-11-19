333a0ad1057ce607c0916b229072e0c144c91dde
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-11-19 16:33:53 +0200

    Added draft of the littlefs system [FS] #17

--- Firmware/firmware_tests/coroutine/coroutine_thoughts.cpp
+++ Firmware/firmware_tests/coroutine/coroutine_thoughts.cpp
@@ -12,11 +12,14 @@
 #include <display/display_coro_compact_gc9a01.hpp>
 #include <display/display_spi_common_coro.hpp>
 #include <logger/logger_service.hpp>
+#include <utils/CoroUtils.hpp>
 
 #include <backends/spi_backend_desktop.hpp>
 #include <spi/spi_wrapper_async_templated.hpp>
 
 #include "st7789_draft.hpp"
+
+#include <lfs.h>
 
 CoroUtils::Task<int> coroutineTask()
 {
@@ -31,6 +34,106 @@ void showFlashDeviceId()
     int testValue = co_await task;
 }
 
+namespace Internals::Fs
+{
+
+class FilesystemPasskey
+{
+    friend class File;
+    friend class Holder;
+
+private:
+    FilesystemPasskey()
+    {
+    }
+    FilesystemPasskey(const FilesystemPasskey&) = default;
+    FilesystemPasskey& operator=(const FilesystemPasskey&) = delete;
+};
+
+class File
+{
+public:
+    explicit File(lfs_file_t fileHandle) : m_pFileHandle{fileHandle}
+    {
+    }
+    ~File()
+    {
+        Holder::Instance().close(this);
+    }
+    CoroUtils::VoidTask write(std::span<const std::uint8_t> dataHolder)
+    {
+        co_return;
+    }
+
+    CoroUtils::Task<std::span<std::uint8_t>> read(std::size_t dataSize)
+    {
+        co_return {};
+    }
+
+    lfs_file_t nativeHandle(const FilesystemPasskey& passkey)
+    {
+        Meta::UnuseVar(passkey);
+        return m_pFileHandle;
+    }
+
+private:
+    lfs_file_t m_pFileHandle;
+};
+class Holder
+{
+public:
+    static Holder& Instance()
+    {
+        static Holder fsHolder{};
+        return fsHolder;
+    }
+
+    CoroUtils::Task<File> openFile(std::string_view path)
+    {
+        lfs_file_t file{};
+        lfs_file_open(&m_fsInstance, &file, "boot_count", LFS_O_RDWR | LFS_O_CREAT);
+        co_return File{file};
+    }
+
+    void close(File* pFile)
+    {
+        assert(pFile);
+        if (!pFile)
+            return;
+        auto nativeHandle = pFile->nativeHandle(FilesystemPasskey{});
+        lfs_file_close(&m_fsInstance, &nativeHandle);
+    }
+
+private:
+    static constexpr inline lfs_config fsConfig{};
+
+private:
+    Holder()
+    {
+        auto error = lfs_mount(&m_fsInstance, &fsConfig);
+        if (error)
+        {
+            lfs_format(&m_fsInstance, &fsConfig);
+            lfs_mount(&m_fsInstance, &fsConfig);
+        }
+    }
+
+private:
+    lfs_t m_fsInstance;
+};
+} // namespace Internals::Fs
+
+CoroUtils::VoidTask fileTest()
+{
+    auto file = co_await Internals::Fs::Holder::Instance().openFile("test.txt");
+    constexpr auto kFileData = std::string_view("Hello world!");
+    co_await file.write(
+        {reinterpret_cast<const std::uint8_t*>(kFileData.data()), kFileData.size()});
+    auto data = co_await file.read(kFileData.size());
+
+    std::cout << std::string_view(reinterpret_cast<const char*>(data.data()), data.size());
+}
+
 int main()
 {
     using TSpiBus = Interface::SpiTemplated::SpiBus<Interface::SpiTemplated::SpiBusDesktopBackend>;
@@ -42,6 +145,7 @@ int main()
     TDisplayDriver compactGc9A01;
     compactGc9A01.fillRectangle(0, 0, 0, 0, nullptr);
     compactGc9A01.initialize();
+
     // ST7789Coroutine displayCoro{
     //       Interface::Spi::createSpiBusAsync<Interface::Spi::SpiInstance::M1>()
     //    ,  240
